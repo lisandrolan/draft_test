dbfebe881377a7bd1c2a8468d8a218f7bbd18943
LISANDROLAN <LISANDROLAN@GMAIL.COM>
2019-01-11 17:12:28 -0300

    react-draft-wysiwyg'

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {EditorState, convertToRaw} from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+
+export default class App extends React.Component {
+  state = {
+    editorState: EditorState.createEmpty(),
+  }
+  onEditorStateChange: Function = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <div>
+        <Editor
+          editorState={editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+        />
+        <textarea
+          disabled
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        />
+      </div>
+    );
+  }
+}
